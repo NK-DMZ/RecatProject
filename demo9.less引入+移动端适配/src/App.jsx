050d5115ce844ffd299ecf9d351bbc720ac49d67
NK-DMZ <2206040225@qq.com>
2021-07-08 22:06:28 +0800

    less and fit

---
+++ demo9.less引入+移动端适配/src/App.jsx
@@ -0,0 +1,19 @@
+/*    App.jsx    */ 
+// import './App.css';
+import './App.less'
+import './untils/rem'
+
+function App() {
+  return (
+    <div className="App">
+        <div className="demo">
+          这是一个项目
+        </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+// yarn add postcss-px2rem customize-cra react-app-rewired
