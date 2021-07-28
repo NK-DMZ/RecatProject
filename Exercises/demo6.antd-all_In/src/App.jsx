2cc0ab5d57425513bb2a2bfdf0e5572ef5ad7dc3
NK-DMZ <2206040225@qq.com>
2021-07-28 08:17:18 +0800

    antd-all_In

---
+++ Exercises/demo6.antd-all_In/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <p >apping</p>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
